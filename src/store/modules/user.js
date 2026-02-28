3ee0f998aafdff15e5c3d1188cc4290c6f59c909
kongklw <18261663365@163.com>
2026-02-28 13:55:05 +0800

    fix info

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -55,7 +55,7 @@ const actions = {
           reject('Verification failed, please Login again.')
         }
 
-        const { roles, name, avatar, introduction } = data
+        const { roles, name, avatar, introduction, id } = data
 
         // roles must be a non-empty array
         if (!roles || roles.length <= 0) {
@@ -64,6 +64,7 @@ const actions = {
 
         commit('SET_ROLES', roles)
         commit('SET_NAME', name)
+        commit('SET_ID', id)
         commit('SET_AVATAR', avatar)
         commit('SET_INTRODUCTION', introduction)
         resolve(data)
